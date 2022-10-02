95a96b3fecebfb1ff85c956583e00028c7d27e3f
dninemfive <11084010+dninemfive@users.noreply.github.com>
2022-10-01 21:21:35 -0700

    generate test map

--- project/170jam1/Assets/MonoBehaviors/Map.cs
+++ project/170jam1/Assets/MonoBehaviors/Map.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,11 +9,21 @@ using UnityEngine;
 public class Map : MonoBehaviour
 {
     Artstyle Artstyle;
+    [HaveComponent(typeof(Enemy))]
     List<GameObject> Enemies;
+    public GameObject TilePrefab;
+    public Board<GameObject> Tiles { get; private set; }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Tiles = new(delegate(int x, int y)
+        {
+            GameObject prefab = Instantiate(TilePrefab);
+            if(x % 2 == y % 2) prefab.GetComponent<MeshRenderer>().material.color = Color.black;
+            // TODO: decide whether to rename all references to use z or to write down that y = z somewhere
+            prefab.transform.position = new(x, transform.position.z, y);
+            return prefab;
+        });
     }
 
     // Update is called once per frame
