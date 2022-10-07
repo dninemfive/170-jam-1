dc11270486090796b120da7bff69d302ee332802
Patrick <pggomez@ucsc.edu>
2022-10-06 21:05:46 -0700

    finished movement
    movement is basically done for now, might need to be tweaked later. no shooting yet.

--- project/170jam1/Assets/MonoBehaviors/TopDownMovement.cs
+++ project/170jam1/Assets/MonoBehaviors/TopDownMovement.cs
@@ -10,6 +10,13 @@ public class TopDownMovement : MonoBehaviour
 
     [SerializeField]
     private float moveSpeed;
+    [SerializeField]
+    private float rotateSpeed;
+    [SerializeField]
+    private bool useMouse;
+
+    [SerializeField]
+    private Camera camera;
 
     private void Awake()
     {
@@ -21,12 +28,43 @@ public class TopDownMovement : MonoBehaviour
     {
         var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
 
-        MoveTowardTarget(targetVector);
+        var movementVector = MoveTowardTarget(targetVector);
+
+        if (!useMouse) {
+            RotateTowardMovementVector(movementVector);
+        } else {
+            RotateTowardMouseVector();
+        }
     }
 
-    private void MoveTowardTarget(Vector3 targetVector)
+    private Vector3 MoveTowardTarget(Vector3 targetVector)
     {
         var speed = moveSpeed * Time.deltaTime;
-        transform.Translate(targetVector * speed);
+        
+        targetVector = Quaternion.Euler(0, camera.gameObject.transform.eulerAngles.y , 0) * targetVector;
+        var targetPosition = transform.position + targetVector  * speed;
+        transform.position = targetPosition;
+        return targetVector;
     }
+
+    private void RotateTowardMovementVector(Vector3 movementVector)
+    {
+        if (movementVector.magnitude == 0) { return; }
+        var rotation = Quaternion.LookRotation(movementVector);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotateSpeed);
+    }
+
+    private void RotateTowardMouseVector()
+    {
+        Ray ray = camera.ScreenPointToRay(_input.MousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
+        {
+            var target = hitInfo.point;
+            target.y = transform.position.y;
+            transform.LookAt(target);
+        }
+    }
+
+
 }
