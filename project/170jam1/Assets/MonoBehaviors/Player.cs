a94a44dc566a89e879f213260ba85082a183d913
dninemfive <11084010+dninemfive@users.noreply.github.com>
2022-10-06 18:30:49 -0700

    fix minor compiler error

--- project/170jam1/Assets/MonoBehaviors/Player.cs
+++ project/170jam1/Assets/MonoBehaviors/Player.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 /// </summary>
 public class Player : MonoBehaviour
 {
-    [HasComponent(typeof(Weapon))]
     public GameObject Weapon;
     Vector3 Position, Velocity;
     // Start is called before the first frame update
