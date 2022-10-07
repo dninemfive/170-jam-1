cf499d76379c43788e23f524db43145eeea61e01
dninemfive <11084010+dninemfive@users.noreply.github.com>
2022-10-06 18:20:34 -0700

    better documentation for Map

--- project/170jam1/Assets/MonoBehaviors/Map.cs
+++ project/170jam1/Assets/MonoBehaviors/Map.cs
@@ -8,31 +8,48 @@ using UnityEngine;
 /// </summary>
 public class Map : MonoBehaviour
 {
+    /// <summary>
+    /// The artstyle used by this specific Map.
+    /// </summary>
     Artstyle Artstyle;
+    /// <summary>
+    /// A list of enemies on this map.
+    /// </summary>
     [HaveComponent(typeof(Enemy))]
     List<GameObject> Enemies;
+    /// <summary>
+    /// A <see cref="Board{GameObject}"/> holding the component tiles of this map.
+    /// </summary>
     public Board<GameObject> Tiles { get; private set; }
+    /// <summary>
+    /// Internal variable used by <see cref="Visible"/>.
+    /// </summary>
     private bool _visible = true;
+    /// <summary>
+    /// Whether or not the component tiles of this map are visible. Setting this modifies all said tiles to match the specified state.
+    /// </summary>
     public bool Visible
     {
         get => _visible;
         set 
         {
             _visible = value;
-            if (Tiles is null) Debug.Log("ti4jae;t");
+            if (Tiles is null) Debug.LogError($"Map at {transform.position} has an uninitialized Tiles object!");
             foreach (GameObject go in Tiles.AllItems)
             {
                 MeshRenderer mr = go.GetComponent<MeshRenderer>();
-                if (mr is null) Debug.Log("null!");
+                if (mr is null) Debug.LogError($"MeshRenderer for tile at {go.transform.position} is null!");
                 else mr.enabled = _visible;
             }
         }
     }
-    // Start is called before the first frame update
+    /// <summary>
+    /// Creates this map and initializes its Tiles; doing the latter causes the component GameObjects to be created.
+    /// </summary>
+    /// <remarks>Called before the first frame update.</remarks>
     void Start()
     {
-        Debug.Log("huh???");
-        Debug.Log(transform.position.z);
+        Debug.Log($"Map initializing at {transform.position}");
         Tiles = new(delegate(int x, int z)
         {
             GameObject prefab = Instantiate(Prefabs.Tile);
@@ -47,16 +64,14 @@ public class Map : MonoBehaviour
         
     }
     /// <summary>
-    /// The desired position of the camera above the map.
+    /// The desired position of the camera above this map, namely centered on the XZ plane and <see cref="GameManager.CAMERA_DISTANCE"/> units above the map.
     /// </summary>
     public Vector3 CameraPosition
     {
         get
         {
-            // center the camera in the x plane
-            float x = transform.position.x + GameManager.NUM_TILES_X / 2f;
+            float x = transform.position.x + GameManager.NUM_TILES_X / 2f + 0.5f;
             float y = transform.position.y + GameManager.CAMERA_DISTANCE;
-            // center the camera in the y plane
             float z = transform.position.z + GameManager.NUM_TILES_Z / 2f - 0.5f;
             return new(x, y, z);
         }
