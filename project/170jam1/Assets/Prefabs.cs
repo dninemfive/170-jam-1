a56660b3d64d020dbc6cbfeaa3da290080f38bb0
dninemfive <11084010+dninemfive@users.noreply.github.com>
2022-10-06 18:27:59 -0700

    better document Prefabs

--- project/170jam1/Assets/Prefabs.cs
+++ project/170jam1/Assets/Prefabs.cs
@@ -7,11 +7,26 @@ using UnityEngine;
 /// </summary>
 public class Prefabs : MonoBehaviour
 {
+    /// <summary>
+    /// The singleton instance of <see cref="GameManager"/>, aliased here for readability.
+    /// </summary>
     public static Prefabs Instance => GameManager.Instance.Prefabs;
+    /// <summary>
+    /// The GameObject prefab corresponding to each instance of <see cref="Map"/>.
+    /// </summary>
     public static GameObject Map => Instance._map;
+    /// <summary>
+    /// Private non-static field backing <see cref="Map"/> so it can be set in the editor.
+    /// </summary>
     [SerializeField]
     private GameObject _map;
+    /// <summary>
+    /// The GameObject prefab corresponding to each component tile of a Map.
+    /// </summary>
     public static GameObject Tile => Instance._tile;
+    /// <summary>
+    /// Private non-static field backing <see cref="Tile"/> so it can be set in the editor.
+    /// </summary>
     [SerializeField]
     private GameObject _tile;
 }
