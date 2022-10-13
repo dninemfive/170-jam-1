97a718b1a63abfe47cca8c64c6579bbf74566ca8
Astric215 <davidiwd215@gmail.com>
2022-10-13 13:28:47 -0700

    basic enemy follow

--- project/170jam1/Assets/MonoBehaviors/Enemy.cs
+++ project/170jam1/Assets/MonoBehaviors/Enemy.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 /// <summary>
 /// MonoBehavior which controls enemies and causes them to move and attack the player.
 /// </summary>
+
+
 public class Enemy : MonoBehaviour
 {
+    ///reference to the player object
+    [SerializeField] GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if(player)
+        {
+            transform.LookAt(player.transform, new Vector3(0, 1, 0));
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, (float)0.009);
+            transform.Rotate(90, 0, 0);
+        }
     }
 }
