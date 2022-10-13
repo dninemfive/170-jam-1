610bb35f435434e9c7764e35634134009a297994
dninemfive <11084010+dninemfive@users.noreply.github.com>
2022-10-12 21:37:45 -0700

    debugging...

--- project/170jam1/Assets/MonoBehaviors/InputHandler.cs
+++ project/170jam1/Assets/MonoBehaviors/InputHandler.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
+    /// <summary>
+    /// The vector corresponding to the player's inputs in the horizontal and vertical directions, respectively.
+    /// </summary>
     public Vector2 InputVector { get; private set; }
-
+    /// <summary>
+    /// The location of the mouse.
+    /// </summary>
     public Vector3 MousePosition { get; private set; }
+    public GameObject Mouse;
 
     // Update is called once per frame
     void Update()
     {
-        var h = Input.GetAxis("Horizontal");
-        var v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
         InputVector = new Vector2(h, v);
 
         MousePosition = Input.mousePosition;
+        Mouse.transform.position = MousePosition;
     }
 }
